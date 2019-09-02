556d61791743cdd1a4ca7715a3d5f11373a4d232
Jack Cherng <jfcherng@gmail.com>
2019-09-02 15:36:21 +0800

    Introduce mypy for static analysis
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ scripts/run_local_tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="${SCRIPT_DIR}/.."
+
+pushd "${PROJECT_DIR}" || exit
+
+mypy -p plugin
+black --check .
+
+popd || exit
