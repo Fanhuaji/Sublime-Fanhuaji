153cff0fd2b931e347d11f3cd5d617365b2432ae
Jack Cherng <jfcherng@gmail.com>
2022-03-07 16:58:10 +0800

    fix: modules should be reloaded when update plugin
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- boot.py
+++ boot.py
@@ -1,3 +1,13 @@
+def plugin_reload() -> None:
+    import sys
+
+    prefix = f"{__package__}."
+    for module_name in tuple(filter(lambda m: m.startswith(prefix) and m != __name__, sys.modules)):
+        del sys.modules[module_name]
+
+
+plugin_reload()
+
 from .plugin import set_up
 from .plugin import tear_down
 from .plugin.commands import *  # noqa: F401, F403
