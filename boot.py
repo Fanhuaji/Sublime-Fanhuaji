b1ef2d53be736fa61f07563fe0ff27b60dfac638
Jun-Fei Cherng <jfcherng@realtek.com>
2026-09-24 10:48:59 +0800

    fix: workaround pydantic core version mismatch
    Signed-off-by: Jun-Fei Cherng <jfcherng@realtek.com>

--- boot.py
+++ boot.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 def reload_plugin() -> None:
     import sys
 
@@ -7,6 +10,24 @@ def reload_plugin() -> None:
         del sys.modules[module_name]
 
 
+def bypass_pydantic_version_check() -> None:
+    """A workaround for https://github.com/jfcherng-sublime/ST-AutoSetSyntax/issues/36"""
+    try:
+        import package_control
+    except ImportError:
+        return
+
+    pydantic_file = Path(package_control.__file__).parent / "pydantic/version.py"
+    data = pydantic_file.read_bytes()
+    data = data.replace(
+        b"def check_pydantic_core_version() -> bool:",
+        b"def  check_pydantic_core_version() -> bool:\n    return True",
+        #     ^ intentional difference to avoid re-replacement next time
+    )
+    pydantic_file.write_bytes(data)
+
+
+bypass_pydantic_version_check()
 reload_plugin()
 
-from .plugin import *  # noqa: F401, F403
+from .plugin import *  # ruff:ignore[undefined-local-with-import-star]
